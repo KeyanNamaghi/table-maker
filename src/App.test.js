a2de1a049cbf66fb7616b56f7122e42f5cb46c27
keyan.namaghi <keyan.namaghi@sainsburys.co.uk>
2022-06-13 20:45:30 +0100

    fix tests

--- src/App.test.js
+++ src/App.test.js
@@ -17,11 +17,9 @@ const images = textBox.match(/\((.*?)\)/g).map((x) => x.slice(1, -1))
 
 test('LayoutToHTML', () => {
   const html = LayoutToHTML(exampleLayout, images)
-  expect(html).toBe(`
-    <table>
-        <tr><th>Before</th><th>After</th></tr>
-        <tr><td><img src='https://user-images.githubusercontent.com/91133983/170726389-8236d7aa-0d91-4c39-8f01-e7d3a752019c.jpeg' /></td><td><img src='https://user-images.githubusercontent.com/91133983/170726524-b6fa4e04-62ba-46c9-add7-9d1ebc493aca.jpeg' /></td></tr><tr><td><img src='https://user-images.githubusercontent.com/91133983/170728615-1acf631b-98ec-4bef-b09f-2c023c1931af.jpeg' /></td><td><img src='https://user-images.githubusercontent.com/91133983/170728683-f9a7885e-eb08-4405-9d4d-2d49959828fe.jpeg' /></td></tr>
-    </table>`)
+  expect(html).toBe(
+    `<table> <tr><th>Before</th><th>After</th></tr> <tr><td><img src='https://user-images.githubusercontent.com/91133983/170726524-b6fa4e04-62ba-46c9-add7-9d1ebc493aca.jpeg'/></td><td><img src='https://user-images.githubusercontent.com/91133983/170728683-f9a7885e-eb08-4405-9d4d-2d49959828fe.jpeg'/></td></tr><tr><td><img src='https://user-images.githubusercontent.com/91133983/170726389-8236d7aa-0d91-4c39-8f01-e7d3a752019c.jpeg'/></td><td><img src='https://user-images.githubusercontent.com/91133983/170728615-1acf631b-98ec-4bef-b09f-2c023c1931af.jpeg'/></td></tr> </table>`,
+  )
 })
 
 test('ImgToMarkdown', () => {
